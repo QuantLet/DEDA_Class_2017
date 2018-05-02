49c6100a6b6564ee6776e4ff28badc90eb7c13be
Junjie Hu <jeremy.junjie.hu@gmail.com>
2018-05-02 18:21:47 +0200

    correct word cloud plot

--- DEDA_Projects/DEDA_WebScrapingAndWordFrequency/stormtrooper_wordcloud_script.py
+++ DEDA_Projects/DEDA_WebScrapingAndWordFrequency/stormtrooper_wordcloud_script.py
@@ -3,22 +3,24 @@
 Created on Sat Nov 18 20:19:52 2017
 
 @author: Julian
+@modified by: Junjie Hu, 02.05.2018
 """
 
 from os import path
+import os
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
 
-d = os.environ['USERPROFILE'] + '\pyning'
-
+root_path = os.getcwd()
 
 # Read the whole text.
-text = open(path.join(d, 'Output.txt')).read()
+with open(path.join(root_path, 'Output.txt'), 'r', encoding='utf-8', errors='ignore') as outout_file:
+    text = outout_file.readlines()
 
 # Mask
-stormtrooper_mask = np.array(Image.open(path.join(d, "stormtrooper_mask.png")))
+stormtrooper_mask = np.array(Image.open(path.join(root_path, "stormtrooper_mask.png")))
 
 # Optional additional stopwords
 stopwords = set(STOPWORDS)
@@ -27,21 +29,19 @@ stopwords.add("said")
 # Construct Word Cloud
 # no backgroundcolor and mode = 'RGBA' create transparency
 wc = WordCloud(max_words=1000, mask=stormtrooper_mask,
-stopwords=stopwords, mode = 'RGBA', background_color=None)
+               stopwords=stopwords, mode='RGBA', background_color=None)
 
 # Pass Text
-wc.generate(text)
+wc.generate(text[0])
 
 # store to file
-wc.to_file(path.join(d, "a_new_hope.png"))
-
+wc.to_file(path.join(root_path, "stormtrooper.png"))
 
 # show
-plt.imshow(wc, interpolation='bilinear')
-plt.axis("off")
-plt.figure()
-plt.imshow(stormtrooper_mask, cmap=plt.cm.gray, interpolation='bilinear')
-plt.axis("off")
-plt.show()
-plt.savefig("stormtrooper.png")
-
+# plt.figure()
+# plt.imshow(wc, interpolation='bilinear')
+# plt.axis("off")
+# plt.imshow(stormtrooper_mask, cmap=plt.cm.gray, interpolation='bilinear')
+# plt.axis("off")
+# plt.savefig("stormtrooper.png")
+# plt.show()
